795fd99aacd6271ffafe25148cdf23da40f2928c
wbean <wbean1990@gmail.com>
2019-03-06 01:34:39 +0800

    server run for accept connection

--- src/main/java/me/wbean/ya/yakv/Application.java
+++ src/main/java/me/wbean/ya/yakv/Application.java
@@ -4,10 +4,18 @@
  */
 package me.wbean.ya.yakv;
 
+import me.wbean.ya.yakv.server.Server;
+
+import java.io.IOException;
+
 /**
  *
  * @author wbean
  * @date 2019/3/5 下午5:51
  */
 public class Application {
+
+    public static void main(String[] args) throws IOException {
+        new Server(8000).start();
+    }
 }
--- src/main/java/me/wbean/ya/yakv/server/Server.java
+++ src/main/java/me/wbean/ya/yakv/server/Server.java
@@ -1,4 +1,67 @@
 package me.wbean.ya.yakv.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Logger;
+
 public class Server {
+    Logger logger = Logger.getLogger(Server.class.getName());
+
+    private volatile AtomicBoolean running;
+
+    ServerSocketChannel serverSocketChannel;
+    public Server(int port) throws IOException {
+        running = new AtomicBoolean(false);
+
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(port));
+        serverSocketChannel.configureBlocking(false);
+    }
+
+    public void start() throws IOException {
+        if(!running.compareAndSet(false, true)){
+            throw new RuntimeException("server already start");
+        }
+
+        Selector connectSelector = Selector.open();
+        serverSocketChannel.register(connectSelector,  SelectionKey.OP_ACCEPT);
+
+        while (true){
+            int readyCount = connectSelector.select(1000);
+            if(readyCount == 0) {
+                logger.info("wait for connect");
+                continue;
+            }
+            Iterator<SelectionKey> selectionKeys = connectSelector.keys().iterator();
+            while (selectionKeys.hasNext()){
+                SelectionKey key = selectionKeys.next();
+                if(key.isConnectable()){
+                    logger.info("connected");
+
+                }else if(key.isAcceptable()){
+                    logger.info("accept");
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+                    socketChannel.read(byteBuffer);
+                    String msg = new String(byteBuffer.array());
+                    logger.info(msg);
+                }else if(key.isReadable()){
+                    logger.info("read");
+                }else if(key.isWritable()){
+                    logger.info("write");
+
+                }
+            }
+        }
+
+
+
+    }
 }
