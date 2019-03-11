86fc312ed11641ca3104b51a8762c647bb58a57b
xiaoqiang <xiaoqiang@youzan.com>
2019-03-11 20:59:49 +0800

    add File Store, Memory Persistent

---
+++ src/main/java/me/wbean/ya/yakv/store/FileStore.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright ©2014-2019 Youzan.com All rights reserved
+ * me.wbean.ya.yakv.store
+ */
+package me.wbean.ya.yakv.store;
+
+/**
+ *
+ * @author wbean
+ * @date 2019/3/11 下午8:41
+ */
+public class FileStore implements Store{
+    @Override
+    public KVEntry get(String key) {
+        return null;
+    }
+
+    @Override
+    public void set(KVEntry kvEntry) {
+
+    }
+
+    @Override
+    public void remove(String key) {
+
+    }
+}
---
+++ src/main/java/me/wbean/ya/yakv/store/MemoryPersistent.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright ©2014-2019 Youzan.com All rights reserved
+ * me.wbean.ya.yakv.store
+ */
+package me.wbean.ya.yakv.store;
+
+/**
+ *
+ * @author wbean
+ * @date 2019/3/11 下午8:53
+ */
+public class MemoryPersistent {
+    public void bgSave(){
+        MemoryStore memoryStore = (MemoryStore)StoreFactory.getStore("MEMORY");
+
+    }
+
+    public void initLoad(){
+
+    }
+}
